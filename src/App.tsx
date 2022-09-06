23657f76dbdbd7938599041b55829372456a3825
ge5rg2 <kghee9612@gmail.com>
2022-09-07 01:02:22 +0900

    3.6 Themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,19 @@
 import React, { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
-  const [value, setValue] = useState("");
-  // event라고 막연하게 작성하면 typescript에선 any로 적용하기 때문에 다음과 같이 적절하게 지정을 해준다
-  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-    // typescript에선 target이 아닌 currentTarget을 사용
-    const {
-      currentTarget: { value },
-    } = event;
-    setValue(value);
-  };
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log(value);
-  };
   return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={onChange}
-          type="text"
-          placeholder="username"
-        />
-        <button>Log in</button>
-      </form>
-    </div>
+    <Container>
+      <H1>Hi</H1>
+    </Container>
   );
 }
 
