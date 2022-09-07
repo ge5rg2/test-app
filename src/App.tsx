bb8a8d0a776f1d298ad3770e32282d8d9f6213b6
ge5rg2 <kghee9612@gmail.com>
2022-09-07 14:31:56 +0900

    4.1 styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,77 @@
 import React, { useState } from "react";
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
 import Router from "./Router";
 
+// 전역으로 적용되는 styled-components 내장 함수 createGlobalStyle 활용해 스타일을 초기화
+const GlobalStyle = createGlobalStyle`
+ @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
+ html, body, div, span, applet, object, iframe,
+ h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+ a, abbr, acronym, address, big, cite, code,
+ del, dfn, em, img, ins, kbd, q, s, samp,
+ small, strike, strong, sub, sup, tt, var,
+ b, u, i, center,
+ dl, dt, dd, menu, ol, ul, li,
+ fieldset, form, label, legend,
+ table, caption, tbody, tfoot, thead, tr, th, td,
+ article, aside, canvas, details, embed,
+ figure, figcaption, footer, header, hgroup,
+ main, menu, nav, output, ruby, section, summary,
+ time, mark, audio, video {
+   margin: 0;
+   padding: 0;
+   border: 0;
+   font-size: 100%;
+   font: inherit;
+   vertical-align: baseline;
+ }
+ /* HTML5 display-role reset for older browsers */
+ article, aside, details, figcaption, figure,
+ footer, header, hgroup, main, menu, nav, section {
+   display: block;
+ }
+ /* HTML5 hidden-attribute fix for newer browsers */
+ *[hidden] {
+     display: none;
+ }
+ body {
+   line-height: 1;
+ }
+ menu, ol, ul {
+   list-style: none;
+ }
+ blockquote, q {
+   quotes: none;
+ }
+ blockquote:before, blockquote:after,
+ q:before, q:after {
+   content: '';
+   content: none;
+ }
+ table {
+   border-collapse: collapse;
+   border-spacing: 0;
+ }
+ * {
+   box-sizing: border-box;
+ }
+ body {
+   font-family: 'Source Sans Pro', sans-serif;
+   background-color:${(props) => props.theme.bgColor};
+   color:${(props) => props.theme.textColor}
+ }
+ a {
+   text-decoration:none;
+ }
+ `;
 function App() {
-  return <Router />;
+  return (
+    // <></>은 fragecomponent 즉 유령 컴포넌트로 div로 감싸줄 필요 없이 해당 문자로 두 태그를 감싸줄 수 있다.
+    <>
+      <GlobalStyle />
+      <Router />
+    </>
+  );
 }
 
 export default App;
--- src/theme.ts
+++ src/theme.ts
@@ -2,7 +2,7 @@ import { DefaultTheme } from "styled-components";
 
 // 미리 타입들을 지정해둬서 추가적인 인자가 들어올 때 알림을 줘서 변화를 캐치하기 슆다
 export const theme: DefaultTheme = {
-  bgColor: "white",
-  textColor: "black",
-  btnColor: "tomato",
+  bgColor: "#f5f6fa",
+  textColor: "#353b48",
+  accentColor: "#9c88ff",
 };
